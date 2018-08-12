e287491acc705c942a00e5d3fae15121c6782429
Pyabecedarian <liu-quan@hotmail.com>
2018-08-12 22:11:37 +0800

    Game: Alien Invasion

---
+++ alien_invasion/settings.py
@@ -0,0 +1,20 @@
+class Settings:
+    """存储 Alien Invasion 的所有设置类"""
+
+    def __init__(self):
+        """初始化游戏设置"""
+
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # 飞船设置
+        self.ship_speed_factor = 2
+
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullet_allowed = 3
